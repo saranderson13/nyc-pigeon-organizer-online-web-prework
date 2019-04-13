88043b9502866df26cf61f9aa3a922e93d380cfc
Sarah Anderson <saranderson13@gmail.com>
2019-04-13 21:41:46 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -12,21 +12,20 @@ def nyc_pigeon_organizer(data)
       # binding.pry
       bird_array.each { |bird_name| pigeon_list[bird_name] = {} }
     end
-    pigeon_list.each do |bird_name, bird_info|
-      information.each do |attribute, birds|
-        if birds.include?(bird_name)
-          if pigeon_list[bird_name][attribute_type] == nil
-            pigeon_list[bird_name][attribute_type] = [attribute.to_s]
-          else
-            pigeon_list[bird_name][attribute_type] << attribute.to_s
-          end
-          # binding.pry
+  end
+  
+  pigeon_list.each do |bird_name, bird_info|
+    information.each do |attribute, birds|
+      if birds.include?(bird_name)
+        if pigeon_list[bird_name][attribute_type] == nil
+          pigeon_list[bird_name][attribute_type] = [attribute.to_s]
+        else
+          pigeon_list[bird_name][attribute_type] << attribute.to_s
         end
       end
-      # binding.pry
     end
-    # binding.pry
   end
+  
   binding.pry
   pigeon_list
   
