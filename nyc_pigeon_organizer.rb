e39219a856465d0db16e5782d53a832c8872fa06
Sarah Anderson <saranderson13@gmail.com>
2019-04-13 21:48:27 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,31 +5,25 @@ def nyc_pigeon_organizer(data)
   pigeon_list = {}
   
   
-  # CREATE HASH CONSISTING OF A NESTED HASH FOR EACH BIRD.
+  # CREATE PIGEON_LIST HASH, WHICH CONTAINS A KEY FOR EACH BIRD.
   data.each do |attribute_type, information|
     information.each do |attribute, birds|
       bird_array = birds.collect { |bird_name| bird_name }
-      # binding.pry
       bird_array.each { |bird_name| pigeon_list[bird_name] = {} }
     end
   end
   
-  pigeon_list.each do |bird_name, bird_info|
-    data.each do |attribute_type, information|
-      information.each do |attribute, birds|
+  # ITERATE OVER NEW PIGEON_LIST HASH
+  # ITERATE OVER ORIGINAL PIGEON_DATA HASH TO COLLECT ANY INFORMATION
+  # CORRESPONDING TO THE GIVEN BIRD
+  pigeon_list.map do |bird_name, bird_info|
+    data.each do |attribute_type, list_of_attributes|
+      list_of_attributes.each do |attribute, birds|
         if birds.include?(bird_name)
-          if pigeon_list[bird_name][attribute_type] == nil
-            pigeon_list[bird_name][attribute_type] = [attribute.to_s]
-          else
-            pigeon_list[bird_name][attribute_type] << attribute.to_s
-          end
+          pigeon_list[bird_name][attribute_type] == nil ? pigeon_list[bird_name][attribute_type] = [attribute.to_s] : pigeon_list[bird_name][attribute_type] << attribute.to_s
         end
       end
     end
   end
-  
-  # binding.pry
-  pigeon_list
-  
-  
+  # pigeon_list
 end
