0326d1c2654dcf9dd76e66d96e76c3a1b7aa0e17
Sarah Anderson <saranderson13@gmail.com>
2019-04-13 20:23:34 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,12 @@
 require 'pry'
+pigeon_list = {}
 
 def nyc_pigeon_organizer(data)
-  data.each do |attribute, information|
-    attribute.collect |
-    binding.pry
+  data.each do |attribute_type, information|
+    pigeon_list[attribute_type] ={}
+    information.each do |attribute, birds|
+      binding.pry
+    end
+    
   end
 end
