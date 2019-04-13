bec74dc6312f3bbeb457a0ab3bbb1a42460ca600
Sarah Anderson <saranderson13@gmail.com>
2019-04-13 21:21:18 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,7 +8,7 @@ def nyc_pigeon_organizer(data)
   # CREATE HASH CONSISTING OF A NESTED HASH FOR EACH BIRD.
   data.each do |attribute_type, information|
     information.each do |attribute, birds|
-      bird_array = birds.collect { |bird_name| bird_name unless bird_array.include?(bird_name) }
+      bird_array = birds.collect { |bird_name| bird_name if bird_array.include?(bird_name) }
       bird_array.each { |bird_name| pigeon_list[bird_name] = {} }
     end
   end
